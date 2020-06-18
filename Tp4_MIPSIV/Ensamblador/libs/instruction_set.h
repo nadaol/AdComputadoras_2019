5618d16343fecace9a24d7ae04bc0c7e9d1b9176
nadaol <nadaol@hotmail.com>
2020-06-18 17:14:22 -0300

    ensamblador done

--- Tp4_MIPSIV/Ensamblador/libs/instruction_set.h
+++ Tp4_MIPSIV/Ensamblador/libs/instruction_set.h
@@ -1,10 +1,3 @@
-/**
-Created on 17/06/2020.
-
- @author jmorales (jmorales@unc.edu.ar)
- @details https://itnext.io/bits-to-bitmaps-a-simple-walkthrough-of-bmp-image-format-765dc6857393
-*/
-
 #ifndef _INSTRUCTION_SET_H_
 #define _INSTRUCTION_SET_H_
 
@@ -16,46 +9,71 @@ Created on 17/06/2020.
 
 ///Number of supported instructions
 #define NSUPORTED_INST 34
+
 ///Register's offset from rigth to left
 #define RS_OFFSET 21
 #define RT_OFFSET 16
 #define RD_OFFSET 11
 #define SA_OFFSET 6
-//Instructions formats
+
+//R-type instructions formats
 #define RDRTSA sscanf(instruction,"%*s %u , %u , %u",&inst->rd,&inst->rt,&inst->sa)
 #define RDRTRS sscanf(instruction,"%*s %u , %u , %u",&inst->rd,&inst->rt,&inst->rs)
 #define RDRSRT sscanf(instruction,"%*s %u , %u , %u",&inst->rd,&inst->rs,&inst->rt)
-#define IFORMAT "%*s"
-#define JFORMAT "%s "
+#define RDRS sscanf(instruction,"%*s %u , %u",&inst->rd,&inst->rs)
+#define RS sscanf(instruction,"%*s %u",&inst->rs)
+///I-type instructions
+#define RTOFFBASE sscanf(instruction,"%*s %u , %u ( %u )",&inst->rt,&inst->immediate,&inst->rs)
+#define RTRSIMM sscanf(instruction,"%*s %u , %u , %u",&inst->rt,&inst->rs,&inst->immediate)
+#define RSRTOFF sscanf(instruction,"%*s %u , %u , %u",&inst->rs,&inst->rt,&inst->immediate)
+#define RTIMM sscanf(instruction,"%*s %u , %u ",&inst->rt,&inst->immediate)
+///J-type instructions
+#define TARGET sscanf(instruction,"%*s %u",&inst->instr_index)
+
 //.coe output line format
 #define OUT_FORMAT "%08x,\n"
 
 void parse_asm(const char* in_file,const char* out_file);
 
+//instruction structures id's
+enum structure_code
+{
+    R_TYPE = 0,
+    I_TYPE = 1,
+    J_TYPE = 2
+};
+
+///Instruction formats id's
 enum format_code
 {
     RDRTSA_FORMAT = 0,
     RDRTRS_FORMAT = 1,
     RDRSRT_FORMAT = 2,
-    I_FORMAT = 3,
-    J_FORMAT = 4
+    RTOFFBASE_FORMAT = 3,
+    RTRSIMM_FORMAT  = 4,
+    RTIMM_FORMAT = 5,
+    RSRTOFF_FORMAT = 6,
+    TARGET_FORMAT = 7,
+    RDRS_FORMAT = 8,
+    RS_FORMAT = 9
 };
 
-///Supported instruction set structure
+///Supported instruction structure
 typedef struct _intruction_set
 {
-    ///Name of supported instruction
+    ///Name of the supported instruction
     const char* name;
+    //Structure specifier
+    enum structure_code structure;
     ///Format specifier
     enum format_code format;
     ///Hexadecimal Opcode/function of instruction
     unsigned int code ;
 }instruction_set;
 
-
 extern instruction_set supported_ins [NSUPORTED_INST];
 
-///MIPSIV instruction set formats
+///MIPSIV instruction set structures
 
 /// R-Type instruction (register)
 typedef struct _R_instruction
--- Tp4_MIPSIV/Ensamblador/libs/parser.c
+++ Tp4_MIPSIV/Ensamblador/libs/parser.c
@@ -7,173 +7,207 @@ instruction_set supported_ins [NSUPORTED_INST] =
 {
     .name = "SLL",
     .format = RDRTSA_FORMAT,
+    .structure = R_TYPE,
     .code = 0x00000000
 },
 {
     .name = "SRL",
     .format = RDRTSA_FORMAT,
+    .structure = R_TYPE,
     .code = 0x00000002
 },
 {
     .name = "SRA",
     .format = RDRTSA_FORMAT,
+    .structure = R_TYPE,
     .code = 0x00000003
 },  
 {
     .name = "SRLV",
     .format = RDRTRS_FORMAT,
+    .structure = R_TYPE,
     .code = 0x00000006
 },  
 {
     .name = "SRAV",
     .format = RDRTRS_FORMAT,
+    .structure = R_TYPE,
     .code = 0x00000007
 },  
 {
     .name = "ADD",
     .format = RDRSRT_FORMAT,
+    .structure = R_TYPE,
     .code = 0x00000020
 },  
 {
     .name = "SLLV",
     .format = RDRTRS_FORMAT,
+    .structure = R_TYPE,
     .code = 0x000000004
 },  
 {
     .name = "SUB",
     .format = RDRSRT_FORMAT,
+    .structure = R_TYPE,
     .code = 0x00000022
 },  
 {
     .name = "AND",
     .format = RDRSRT_FORMAT,
+    .structure = R_TYPE,
     .code = 0x00000024
 },  
 {
     .name = "OR",
     .format = RDRSRT_FORMAT,
+    .structure = R_TYPE,
     .code = 0x00000025
 },  
 {
     .name = "XOR",
     .format = RDRSRT_FORMAT,
+    .structure = R_TYPE,
     .code = 0x00000026
 },  
 {
     .name = "NOR",
     .format = RDRSRT_FORMAT,
+    .structure = R_TYPE,
     .code = 0x00000027
 }, 
 {
     .name = "SLT",
     .format = RDRSRT_FORMAT,
+    .structure = R_TYPE,
     .code = 0x0000002A
 },
 ///I-type supported Instructions (code = opcode)
 {
     .name = "LB",
-    .format = I_FORMAT,
+    .format = RTOFFBASE_FORMAT,
+    .structure = I_TYPE,
     .code = 0x80000000
 },
 {
     .name = "LH",
-    .format = I_FORMAT,
+    .format = RTOFFBASE_FORMAT,
+    .structure = I_TYPE,
     .code = 0x84000000
 },
 {
     .name = "LW",
-    .format = I_FORMAT,
+    .format = RTOFFBASE_FORMAT,
+    .structure = I_TYPE,
     .code = 0X8C000000
 },
 {
     .name = "LWU",
-    .format = I_FORMAT,
+    .format = RTOFFBASE_FORMAT,
+    .structure = I_TYPE,
     .code = 0X9C000000
 },
 {
     .name = "LBU",
-    .format = I_FORMAT,
+    .format = RTOFFBASE_FORMAT,
+    .structure = I_TYPE,
     .code = 0X90000000
 },
 {
     .name = "LHU",
-    .format = I_FORMAT,
+    .format = RTOFFBASE_FORMAT,
+    .structure = I_TYPE,
     .code = 0X94000000
 },
 {
     .name = "SB",
-    .format = I_FORMAT,
+    .format = RTOFFBASE_FORMAT,
+    .structure = I_TYPE,
     .code = 0XA0000000
 },
 {
     .name = "SH",
-    .format = I_FORMAT,
+    .format = RTOFFBASE_FORMAT,
+    .structure = I_TYPE,
     .code = 0XA4000000
 },
 {
     .name = "SW",
-    .format = I_FORMAT,
+    .format = RTOFFBASE_FORMAT,
+    .structure = I_TYPE,
     .code = 0XAC000000
 },
 {
     .name = "ADDI",
-    .format = I_FORMAT,
+    .format = RTRSIMM_FORMAT,
+    .structure = I_TYPE,
     .code = 0X20000000
 },
 {
     .name = "ANDI",
-    .format = I_FORMAT,
+    .format = RTRSIMM_FORMAT,
+    .structure = I_TYPE,
     .code = 0X30000000
 },
 {
     .name = "ORI",
-    .format = I_FORMAT,
+    .format = RTRSIMM_FORMAT,
+    .structure = I_TYPE,
     .code = 0X34000000
 },
 {
     .name = "XORI",
-    .format = I_FORMAT,
+    .format = RTRSIMM_FORMAT,
+    .structure = I_TYPE,
     .code = 0X38000000
 },
 {
     .name = "LUI",
-    .format = I_FORMAT,
+    .format = RTIMM_FORMAT,
+    .structure = I_TYPE,
     .code = 0X3C000000
 },
 {
     .name = "SLTI",
-    .format = I_FORMAT,
+    .format = RTRSIMM_FORMAT,
+    .structure = I_TYPE,
     .code = 0X28000000
 },
 {
     .name = "BEQ",
-    .format = I_FORMAT,
+    .format = RSRTOFF_FORMAT,
+    .structure = I_TYPE,
     .code = 0X10000000
 },
 {
     .name = "BNE",
-    .format = I_FORMAT,
+    .format = RSRTOFF_FORMAT,
+    .structure = I_TYPE,
     .code = 0X14000000
 },
 {
     .name = "J",
-    .format = I_FORMAT,
+    .format = TARGET_FORMAT,
+    .structure = J_TYPE,
     .code = 0X08000000
 },
 {
     .name = "JAL",
-    .format = I_FORMAT,
+    .format = TARGET_FORMAT,
+    .structure = J_TYPE,
     .code = 0X0C000000
 },
 ///J-TYPE supported instructions (code = last 6 bits)
 {
     .name = "JR",
-    .format = J_FORMAT,
+    .format = RS_FORMAT,
+    .structure = R_TYPE,
     .code = 0X00000008
 },
 {
     .name = "JALR",
-    .format = J_FORMAT,
+    .format = RDRS_FORMAT,
+    .structure = R_TYPE,
     .code = 0X00000009
 },
 };
@@ -210,40 +244,72 @@ void write_inst(char* instruction , FILE* fp_out ,int line_num)
         fprintf(stderr,"Error Invalid instruction at line %d\n",line_num);
         exit(EXIT_FAILURE);
     }
-    switch (supported_ins[inst_index].format)
+    int format = supported_ins[inst_index].format;
+    switch (supported_ins[inst_index].structure)
     {
-
-    case RDRSRT_FORMAT : case RDRTRS_FORMAT : case RDRTSA_FORMAT:
-        {   
+        case R_TYPE :
+        {
             R_instruction* inst = calloc(1,sizeof(R_instruction));
-            //inst->function = supported_ins[inst_index].code;
-            int ret;
-            if(supported_ins[inst_index].format == RDRTSA_FORMAT)ret=RDRTSA;
-            else if (supported_ins[inst_index].format == RDRSRT_FORMAT)ret=RDRSRT;
-            else ret=RDRTRS;
-            if(ret<3 || inst->rd > 31 || inst->rs > 31 || inst->rt > 31 || inst->sa > 31)
+            int ret,narg = 0;
+            if(format == RDRTSA_FORMAT){ret=RDRTSA;narg=3;}
+            else if (format == RDRSRT_FORMAT){ret=RDRSRT;narg=3;}
+            else if(format == RDRS_FORMAT){ret=RDRS;narg=2;}
+            else if(format == RDRTRS_FORMAT){ret=RDRTRS;narg=3;}
+            else if(format == RS_FORMAT){ret = RS;narg=1;}
+            else  ret=-1;
+            if(ret<narg || inst->rd > 0X1F || inst->rs > 0X1F || inst->rt > 0X1F || inst->sa > 0X1F)
             {
                 fprintf(stderr,"Error Invalid instruction at line %d\n",line_num);
                 exit(EXIT_FAILURE);    
             }
-            unsigned int instruction = inst->opcode + (inst->rs << RS_OFFSET) + (inst->rt << RT_OFFSET) + (inst->rd << RD_OFFSET) + (inst->sa << SA_OFFSET) + supported_ins[inst_index].code;
+            unsigned int instruction = (inst->rs << RS_OFFSET) + (inst->rt << RT_OFFSET) + (inst->rd << RD_OFFSET) + (inst->sa << SA_OFFSET) + supported_ins[inst_index].code;
             char instruction_hex [WORD_LENGTH] ;
             sprintf(instruction_hex,OUT_FORMAT,instruction);
-            printf("instruction : %s , rd : %d rt : %d rs : %d sa : %d code : %s\n",instruction_name,inst->rd,inst->rt,inst->rs,inst->sa,instruction_hex);
+            //printf("instruction : %s , rd : %d rt : %d rs : %d sa : %d code : %s\n",instruction_name,inst->rd,inst->rt,inst->rs,inst->sa,instruction_hex);
             fwrite(&instruction_hex,sizeof(char),10,fp_out);
             break;
         }
-
-    case I_FORMAT:
-        //I_instruction* inst = calloc(1,sizeof(I_instruction));
-        break;
-
-    case J_FORMAT:
-        /* code */
-        break;
-    
-    default:
-        printf("Error not implemented format\n");
+        case I_TYPE :
+        {
+            I_instruction* inst = calloc(1,sizeof(R_instruction));
+            int ret,narg = 0;
+            if(format == RTOFFBASE_FORMAT){ret=RTOFFBASE;narg=3;}
+            else if (format == RTRSIMM_FORMAT){ret=RTRSIMM;narg=3;}
+            else if(format == RSRTOFF_FORMAT){ret=RSRTOFF;narg=3;}
+            else if(format == RTIMM_FORMAT){ret=RTIMM;narg=2;}
+            else  ret = -1;
+             if(ret<narg || inst->rs > 0X1F || inst->rt > 0X1F || inst->immediate > 0XFFFF)
+            {
+                fprintf(stderr,"Error Invalid instruction at line %d\n",line_num);
+                exit(EXIT_FAILURE);
+            }
+            unsigned int instruction = supported_ins[inst_index].code + (inst->rs << RS_OFFSET) + (inst->rt << RT_OFFSET) + (inst->immediate);
+            char instruction_hex [WORD_LENGTH] ;
+            sprintf(instruction_hex,OUT_FORMAT,instruction);
+            //printf("instruction : %s , rd : %d rt : %d rs : %d sa : %d code : %s\n",instruction_name,inst->rd,inst->rt,inst->rs,inst->sa,instruction_hex);
+            fwrite(&instruction_hex,sizeof(char),10,fp_out);
+            break;
+        }
+        case J_TYPE :
+        {
+            J_instruction* inst = calloc(1,sizeof(R_instruction));
+            int ret,narg = 0;
+            if(format == TARGET_FORMAT){ret=TARGET;narg=1;}
+            else  ret = -1;
+            if(ret<narg || inst->instr_index > 0x3FFFFFF)
+            {
+                fprintf(stderr,"Error Invalid instruction at line %d\n",line_num);
+                exit(EXIT_FAILURE);
+            }
+            unsigned int instruction = supported_ins[inst_index].code + (inst->instr_index);
+            char instruction_hex [WORD_LENGTH] ;
+            sprintf(instruction_hex,OUT_FORMAT,instruction);
+            //printf("instruction : %s , rd : %d rt : %d rs : %d sa : %d code : %s\n",instruction_name,inst->rd,inst->rt,inst->rs,inst->sa,instruction_hex);
+            fwrite(&instruction_hex,sizeof(char),10,fp_out);
+            break;
+        }
+        default:
+        printf("Error not implemented structure\n");
         exit(EXIT_FAILURE);
     }
 
